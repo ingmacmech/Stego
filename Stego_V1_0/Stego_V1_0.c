847054b3efdd1a1625509b62f5ec7a507a81b786
aschmocker <schmocker@puzzle.ch>
2017-01-20 14:36:57 +0100

    Ported to Linux

--- Stego_V1_0/Stego_V1_0.c
+++ Stego_V1_0/Stego_V1_0.c
@@ -23,7 +23,7 @@
 #include <string.h>
 #include "divA.h"
 
-
+#define clear()         printf("\033[H\033[J") // clear the screen
 
 typedef unsigned char byte;
 FILE *source, *stego_rb, *stego_wb, *textOutput, *textInput;
@@ -33,10 +33,9 @@ byte puffer;
 
 void openStegoPic_rb()
 {
-    stego_rb = fopen("Output\\Decrypt_Pic.bmp", "rb");
+    stego_rb = fopen("Output/Decrypt_Pic.bmp", "rb");
 
-    if (stego_rb== NULL)
-    {
+    if (stego_rb== NULL) {
         printf("------------------------------------------------\n");
         printf("Failed to open file: \"Decrypt_Pic.bmp\"\n");
         printf("Please check name of picture in file: \"Output\"\n");
@@ -48,15 +47,14 @@ void openStegoPic_rb()
 
 void openStegoPic_wb()
 {
-    stego_wb = fopen("Output\\Decrypt_Pic.bmp", "wb");
+    stego_wb = fopen("Output/Decrypt_Pic.bmp", "wb");
 }
 
 void openOrginalPic()
 {
-    source = fopen("Input\\Orginal_Pic.bmp", "rb");
+    source = fopen("Input/Orginal_Pic.bmp", "rb");
 
-    if (source== NULL)
-    {
+    if (source== NULL) {
         printf("------------------------------------------------\n");
         printf("Failed to open file: \"Orginal_Picture.bmp\"\n");
         printf("Please check name of picture in file: \"Input\"\n");
@@ -68,10 +66,9 @@ void openOrginalPic()
 
 void openInputText()
 {
-    textInput=fopen("Input\\Text_Input.txt","r");
+    textInput=fopen("Input/Text_Input.txt","r");
 
-    if (textInput== NULL)
-    {
+    if (textInput== NULL) {
         printf("------------------------------------------------\n");
         printf("Failed to open file: \"Text_Input.txt\"\n");
         printf("Please check name of .txt  in file: \"Input\"\n");
@@ -83,16 +80,26 @@ void openInputText()
 
 void openOutputText()
 {
-    textOutput = fopen("Output\\Text_Output.txt","w");
+    textOutput = fopen("Output/Text_Output.txt","w");
 }
 
 void closeFile(int n)
 {
-    if(n==1)fclose (stego_rb);
-    if(n==2)fclose (stego_wb);
-    if(n==3)fclose (source);
-    if(n==4)fclose (textOutput);
-    if(n==5)fclose (textInput);
+    if(n==1) {
+        fclose (stego_rb);
+    }
+    if(n==2) {
+        fclose (stego_wb);
+    }
+    if(n==3) {
+        fclose (source);
+    }
+    if(n==4) {
+        fclose (textOutput);
+    }
+    if(n==5) {
+        fclose (textInput);
+    }
 }
 
 /***************************************
@@ -116,8 +123,7 @@ void writeTextLength(int charNr)
 {
     int k, mask=0;
 
-    for (k=0;k<16;k++)
-    {
+    for (k=0; k<16; k++) {
         fread(&puffer,sizeof(byte),1,source);
         puffer = puffer & 0xFC;
         mask= charNr & 0x03;
@@ -138,8 +144,7 @@ void writeEncryptData(byte oneLetter)
     byte mask=0;
     int k;
 
-    for (k=0;k<4;k++)
-    {
+    for (k=0; k<4; k++) {
         fread(&puffer,sizeof(byte),1,source);
         puffer = puffer & 0xFC;
         mask= oneLetter & 0x03;
@@ -163,8 +168,7 @@ void readTextLength()
 {
     int k,last2bit=0;
 
-    for (k=0;k<31;k=k+2)
-    {
+    for (k=0; k<31; k=k+2) {
         fread(&puffer,sizeof(byte),1,stego_rb);
         last2bit=puffer & 0x03;
         last2bit=last2bit<<k;
@@ -184,8 +188,7 @@ void readEncryptData()
     int k;
     byte last2bit=0, oneLetter=0;
 
-    for (k=0;k<7;k=k+2)
-    {
+    for (k=0; k<7; k=k+2) {
         fread(&puffer,sizeof(byte),1,stego_rb);
         last2bit=puffer & 0x03;
         last2bit=last2bit<<k;
@@ -209,12 +212,11 @@ void testLength()
 {
     textLength = strlen(shortText);
 
-/* shortText hat Platz für 301 Zeichen
-   Die 301.Stelle wird benötigt, um auf eine zu grosse Eingabe
-   des Benuzers zu prüfen*/
+    /* shortText hat Platz für 301 Zeichen
+       Die 301.Stelle wird benötigt, um auf eine zu grosse Eingabe
+       des Benuzers zu prüfen*/
 
-    if(textLength>300)
-    {
+    if(textLength>300) {
         empty_stdin();
         printf("-----------------------------------------------\n");
         printf("ERROR!!!!! Text is too long\n\a");
@@ -236,25 +238,29 @@ void simpleEncrypt()
     int j=0,i=0;
     byte oneLetter;
 
-    system("cls");
+    clear();
     openOrginalPic();
     openStegoPic_wb();
 
     consoleText();
-    system("cls");
+    clear();
     testLength();
 
-    while (!feof(source))
-    {
-        if(i<=53) writePicInf();
-        if(i==54) writeTextLength(textLength);
-        if (i>54 && i<=54+textLength)
-        {
+    while (!feof(source)) {
+        if(i<=53) {
+            writePicInf();
+        }
+        if(i==54) {
+            writeTextLength(textLength);
+        }
+        if (i>54 && i<=54+textLength) {
             oneLetter=shortText[j];
             writeEncryptData(oneLetter);
             j++;
         }
-        if (i>=54+textLength) writePicInf();
+        if (i>=54+textLength) {
+            writePicInf();
+        }
         i++;
     }
     closeFile(2);
@@ -262,7 +268,7 @@ void simpleEncrypt()
 
     printf("-------------------------------------------------------------\n");
     printf("Your text is successfully encrypted\n");
-    printf("You find the picture in \\Stego_V1.0\\Output\\Decrypt_Pic.bmp\n");
+    printf("You find the picture in \\Stego_V1.0\\Output/Decrypt_Pic.bmp\n");
     printf("--------------------------------------------------------------\n\n");
 }
 
@@ -291,23 +297,27 @@ void textFileEncrypt()
     int i=0, fileLength;
     byte textLetter;
 
-    system("cls");
+    clear();
     fileLength = testTextLength();
 
     openOrginalPic();
     openStegoPic_wb();
     openInputText();
 
-    while(!feof(source))
-    {
-        if(i<=53) writePicInf();
-        if(i==54) writeTextLength(fileLength);
-        if(i>54 && i<=54+fileLength)
-        {
+    while(!feof(source)) {
+        if(i<=53) {
+            writePicInf();
+        }
+        if(i==54) {
+            writeTextLength(fileLength);
+        }
+        if(i>54 && i<=54+fileLength) {
             fread(&textLetter,sizeof(byte),1,textInput);
             writeEncryptData(textLetter);
         }
-        if (i>=54+fileLength) writePicInf();
+        if (i>=54+fileLength) {
+            writePicInf();
+        }
         i++;
     }
     closeFile(2);
@@ -316,7 +326,7 @@ void textFileEncrypt()
 
     printf("----------------------------------------------------------\n");
     printf("Your text is successfully encrypted\n");
-    printf("You find the picture in \\Stego_V1.0\\Output\\Decrypt_Pic.bmp\n");
+    printf("You find the picture in \\Stego_V1.0\\Output/Decrypt_Pic.bmp\n");
     printf("----------------------------------------------------------\n");
 }
 /****************************************
@@ -330,16 +340,23 @@ void decrypt()
 {
     int i=0;
 
-    system("cls");
+    clear();
     openStegoPic_rb();
     openOutputText();
 
-    while (!feof(stego_rb))
-    {
-        if(i<=53) readPicInf();
-        if (i==54)readTextLength();
-        if(i>54 && i<=54+readLength) readEncryptData();
-        if(i>54+readLength) readPicInf();
+    while (!feof(stego_rb)) {
+        if(i<=53) {
+            readPicInf();
+        }
+        if (i==54) {
+            readTextLength();
+        }
+        if(i>54 && i<=54+readLength) {
+            readEncryptData();
+        }
+        if(i>54+readLength) {
+            readPicInf();
+        }
         i++;
     }
     closeFile(1);
@@ -366,22 +383,27 @@ int displayMainMenu ()
     printf ("-----------------------------------------\n\n");
     printf ("Enter your choice : ");
     check=scanf("%i", &value);
-    system("cls");
+    clear();
 
-    if (check==1)
-    {
-        if(value==0) return value;
-        if(value==1) return value;
-        if(value==2) return value;
-        if(value==3) return value;
+    if (check==1) {
+        if(value==0) {
+            return value;
+        }
+        if(value==1) {
+            return value;
+        }
+        if(value==2) {
+            return value;
+        }
+        if(value==3) {
+            return value;
+        }
         printf("-----------------\n");
         printf("Wrong choise!!!!!\n");
         printf("Pleas try again\n");
         printf("-----------------\n\n");
         displayMainMenu();
-    }
-    else
-    {
+    } else {
         empty_stdin();
         printf("-----------------\n");
         printf("Wrong choise!!!!!\n");
@@ -395,13 +417,20 @@ int main()
 {
     int menuItem = 0;
 
-	while (1)
-	{
-		menuItem = displayMainMenu();
-		if (menuItem == 0) exit(0);
-		if (menuItem == 1) simpleEncrypt();
-		if (menuItem == 2) textFileEncrypt();
-		if (menuItem == 3) decrypt();
+    while (1) {
+        menuItem = displayMainMenu();
+        if (menuItem == 0) {
+            exit(0);
+        }
+        if (menuItem == 1) {
+            simpleEncrypt();
+        }
+        if (menuItem == 2) {
+            textFileEncrypt();
+        }
+        if (menuItem == 3) {
+            decrypt();
+        }
     }
     return 0;
 }
